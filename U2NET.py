b94782d0ccf6a129c3cf98b8c1a8ae5884391e9c
Harvey.Ye <hn85469388@hotmail.com>
2022-02-23 15:39:15 +0800

    U2NET

--- U2NET.py
+++ U2NET.py
@@ -1,1 +1,12 @@
-import numpy as np
+import torch
+from torch import nn
+from torchvision import models
+import torch.nn.functional as F
+
+class Convalution(nn.Module):
+    def __init__(self, in_ch=3, out_ch=3,dirate = 1):
+        super(Convalution,self).__init__()
+
+        self.conv = nn.Conv2d(in_ch, out_ch, 3,padding=dirate, dilation=dirate)
+        self.bn = nn.BatchNorm2d(out_ch)
+        self.relu = nn.ReLU(inplace=True)
