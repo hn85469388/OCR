50fa9b1ae5ea80d9478f94a6434d96de438e394c
Harvey.Ye <hn85469388@hotmail.com>
2022-03-03 14:55:34 +0800

    U2NET

--- U2NET.py
+++ U2NET.py
@@ -355,7 +355,6 @@ class U2NET(nn.Module):
         self.outconv = nn.Conv2d(6*out_ch, out_ch, 1)
 
     def forward(self, x):
-
         hx = x
 
         #stage 1
@@ -418,7 +417,8 @@ class U2NET(nn.Module):
 
         d0 = self.outconv(torch.cat((d1, d2, d3, d4, d5, d6), 1))
 
-        return F.sigmoid(d0),  F.sigmoid(d1),  F.sigmoid(d2),  F.sigmoid(d3),  F.sigmoid(d4),  F.sigmoid(d5),  F.sigmoid(d6)
+        return F.sigmoid(d0),  F.sigmoid(d1),  F.sigmoid(d2),  F.sigmoid(d3),  F.sigmoid(d4),  F.sigmoid(d5),  \
+               F.sigmoid(d6)
 
 if __name__ == "__main__":
     u2net = U2NET()
