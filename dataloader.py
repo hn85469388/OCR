23e183c30730db2370d4c403cf68f416ccb09b47
Harvey.Ye <hn85469388@hotmail.com>
2022-03-08 18:05:32 +0800

    data processing

--- dataloader.py
+++ dataloader.py
@@ -2,9 +2,10 @@ from torch.utils.data import Dataset
 import os
 from PIL import Image
 from torchvision import transforms
+import cv2
 
-data_dir = ''
-label_dir = ''
+data_dir = r'D:/SideProject/DUTS/DUTS-TR-Image/'
+label_dir =r'D:/SideProject/DUTS/DUTS-TR-Mask/'
 
 class DataSet(Dataset):
     def __init__(self):
@@ -12,12 +13,16 @@ class DataSet(Dataset):
         self.dataset = os.listdir(data_dir)
         self.dataset = self.dataset
 
-    def __getitem(self, index):
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
         try:
-            image = Image.open(os.path.join(data_dir, self.dataset[index])).convert('RGB')
-            label = Image.open(os.path.join(label_dir, self.dataset[index])).convert('L')
-            pad = max(image,label)
-            size = (pad,pad)
+            # imagePath = self.dataset[index]
+            image = Image.open(os.path.join(data_dir, self.dataset[index]))
+            label = Image.open(os.path.join(label_dir, self.dataset[index]))
+            pad = max(image, label)
+            size = (pad, pad)
             transform = transforms.Compose([
                 transforms.CenterCrop(size),
                 transforms.Resize(490),
@@ -25,7 +30,13 @@ class DataSet(Dataset):
             ])
             imagedata = transform(image)
             labeldata = transform(label)
+            # image = cv2.imread(imagePath)
+            # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            # mask = cv2.imread()
 
             return imagedata,labeldata
-        except:
-            return self.__getitem(index+1)
+
+        except Exception as e:
+            print(e)
+
+            # return self.__getitem__(index + 1)
